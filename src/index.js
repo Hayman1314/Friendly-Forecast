1ffc1977f8286c9b4fc384b2591bca919525c5a8
Hayman1314 <Haymanmyatnoe.spn@gmail.com>
2025-02-12 16:45:05 +0630

    Change some padding of weather data in css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ function getWeather(response) {
   let temperatureElement = document.querySelector(".temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector(".city");
+
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
