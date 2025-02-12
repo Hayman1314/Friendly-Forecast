134a89193fcc24929c1f6454b07254b46ed855e7
Hayman1314 <Haymanmyatnoe.spn@gmail.com>
2025-02-12 14:33:43 +0630

    Added js, a basic search engine and change a few html and css in city element.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector(".city");
+  cityElement.innerHTML = searchInput.value;
+}
+let searchForm = document.querySelector(".search-form");
+searchForm.addEventListener("submit", searchCity);
